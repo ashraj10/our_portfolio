bd7108639e481374f7b49d1424172dc1bca14d4b
yeshraj <yashrajkhirwar@gmail.com>
2025-09-28 11:24:50 +0530

    changes

--- src/app/components/navbar/page.jsx
+++ src/app/components/navbar/page.jsx
@@ -69,6 +69,7 @@ export default function Navbar() {
         </div>
 
         {/* Mobile Nav Links */}
+        {/* changes */}
         <ul className="flex flex-col p-6 space-y-4">
           {navLinks.map((link) => (
             <li key={link}>
